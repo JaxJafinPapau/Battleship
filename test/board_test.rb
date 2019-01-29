84bdf264ee3ad94d774e90d291a74a20002f193a
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-01-29 15:52:25 -0700

    Initial commit

---
+++ test/board_test.rb
@@ -0,0 +1,15 @@
+require './lib/cell.rb'
+require './lib/ship.rb'
+require './lib/board.rb'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class BoardTest
+
+  def setup
+    cruiser = Ship.new("Cruiser", 3)
+  end
+
+
+end
