176fae2c17861d3116a42f18567a74b2c75a6622
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-01-28 18:34:12 -0700

    Removed health method, initialized @health = @length

--- lib/ship.rb
+++ lib/ship.rb
@@ -2,23 +2,20 @@ require 'pry'
 
 class Ship
   attr_reader :name,
-              :length
+              :length,
               :health
 
   def initialize(name, length)
     @name = name
     @length = length
-  end
-
-  def health
-    @length
+    @health = @length
   end
 
   def sunk?
-    health <= 0
+    @health <= 0
   end
 
   def hit
-    health -=1
+    @health -=1
   end
 end
