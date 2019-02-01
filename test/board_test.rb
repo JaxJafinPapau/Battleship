1eca3234f09a5c5ed19c9b10cbcf56ce62d0f960
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-01-31 22:02:56 -0700

    Adds row_neighbors test

--- test/board_test.rb
+++ test/board_test.rb
@@ -38,6 +38,12 @@ class BoardTest < Minitest::Test
     assert_equal column_neighbors, @board.column_neighbors
   end
 
+  def test_does_row_neighbors_give_neighbors
+    row_neighbors = [["A1", "A2"], ["A2", "A3"], ["A3", "A4"], ["A4", "B1"], ["B1", "B2"], ["B2", "B3"], ["B3", "B4"], ["B4", "C1"], ["C1", "C2"], ["C2", "C3"], ["C3", "C4"], ["C4", "D1"], ["D1", "D2"], ["D2", "D3"], ["D3", "D4"]]
+
+    assert_equal row_neighbors, @board.row_neighbors
+  end
+
   def test_does_board_validate_horizontal_placement
     skip
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
