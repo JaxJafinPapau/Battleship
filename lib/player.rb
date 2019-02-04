92a30d1699d023f0a7c742c819340c17864c888d
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-04 15:53:39 -0700

    Creates player class and test with skeletons

---
+++ lib/player.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
+class Player
+
+
+
+end
---
+++ test/player_test.rb
@@ -0,0 +1,15 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/player'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+require 'pry'
+
+class PlayerTest
+
+
+
+
+
+end
