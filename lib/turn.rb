c1a2c712efe046e7a8608854eb1407f8431451a5
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-05 11:22:03 -0700

    Adds give_feedback method and test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,32 @@
 class Turn
+  attr_reader :own_board,
+              :opponent_board
 
   def initialize
-  end 
+    @own_board = Board.new
+    @opponent_board = Board.new
+  end
 
+
+  def display_own_board
+    @own_board.render(true)
+  end
+
+  def display_opponent_board
+    @opponent_board.render
+  end
+
+  def take_shot(coordinate)
+    @opponent_board.cells[coordinate].fire_upon
+  end
+
+  def give_feedback(coordinate)
+    if @opponent_board.cells[coordinate].render == "M"
+      return "Miss!"
+    elsif @opponent_board.cells[coordinate].render == "H"
+      return "Hit!"
+    else @opponent_board.cells[coordinate].render == "X"
+      return "Sunk!"
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -3,16 +3,69 @@ require './lib/turn'
 require './lib/cell'
 require './lib/ship'
 require './lib/board'
+require 'Minitest/autorun'
+require 'Minitest/pride'
 
-class Turn < Minitest::Test
+class TurnTest < Minitest::Test
 
   def setup
     @turn = Turn.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
-
-    assert_instance_of turn, Turn
+    assert_instance_of Turn, @turn
   end
 
+  def test_turn_displays_own_board
+    @turn.own_board.place(@cruiser, ["A1", "A2", "A3"])
+    @turn.own_board.place(@submarine, ["B4", "C4"])
+
+    sample_board = "  1 2 3 4 \n" +
+                   "A S S S . \n" +
+                   "B . . . S \n" +
+                   "C . . . S \n" +
+                   "D . . . . \n"
+
+    assert_equal sample_board, @turn.display_own_board
+  end
+
+  def test_turn_displays_opponent_board
+    @turn.opponent_board.place(@cruiser, ["D1", "D2", "D3"])
+    @turn.opponent_board.place(@submarine, ["A4", "B4"])
+
+    sample_board = "  1 2 3 4 \n" +
+                   "A . . . . \n" +
+                   "B . . . . \n" +
+                   "C . . . . \n" +
+                   "D . . . . \n"
+
+    assert_equal sample_board, @turn.display_opponent_board
+  end
+
+  def test_turn_takes_shot
+    @turn.opponent_board.place(@cruiser, ["D1", "D2", "D3"])
+    @turn.take_shot("B1")
+    @turn.take_shot("D1")
+
+    sample_board = "  1 2 3 4 \n" +
+                   "A . . . . \n" +
+                   "B M . . . \n" +
+                   "C . . . . \n" +
+                   "D H . . . \n"
+
+    assert_equal sample_board, @turn.display_opponent_board
+  end
+
+  def test_turn_gives_feedback
+    @turn.opponent_board.place(@cruiser, ["D1", "D2", "D3"])
+    @turn.take_shot("D1")
+    expect_hit = @turn.give_feedback("D1")
+    @turn.take_shot("B1")
+    expect_miss = @turn.give_feedback("B1")
+
+    assert_equal "Hit!", expect_hit
+    assert_equal "Miss!", expect_miss
+  end
 end
