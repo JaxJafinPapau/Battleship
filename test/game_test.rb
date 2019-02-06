ce4e7bc002a75d5523fc503ab31580aa48dc4946
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-02-06 16:24:37 -0700

    updates to game and game_test

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,18 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
-require './lib/cell'
-require './lib/ship'
-require './lib/board'
+#require './lib/cell'
+#require './lib/ship'
+#require './lib/board'
 require './lib/game'
 
 class GameTest < Minitest::Test
 
   def setup
     @game = Game.new
-    @cell = Cell.new("B4")
-    @ship = Ship.new("skiff", 1)
-    @board = Board.new
+    #@cell = Cell.new("B4")
+    #@ship = Ship.new("skiff", 1)
+    #@board = Board.new
   end
 
   def test_it_exists
@@ -21,21 +21,24 @@ class GameTest < Minitest::Test
   end
 
   def test_main_menu
-    @game
 
     assert_equal "Welcome to BATTLESHIP. Enter p to play. Enter q to quit.", @game.main_menu
   end
 
-#  def test_player_input
-#
+  def test_player_wants_to_play
+    answer = "p"
 
-#  end
+    assert_equal "p", @game.play_or_quit(true)
+  end
 
-  #def test_computer_place_ships
-  #  skip
-  #end
+  def test_player_wants_to_quit
+    answer = "q"
 
-  #def test_player_place_ships
-  #  skip
+    assert_equal exit, @game.play_or_quit
+  end
+
+  #def test_end_game
+
+  #  assert_equal , @game.end_game
   #end
 end
